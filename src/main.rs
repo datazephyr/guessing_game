6097f2d2fdb571dd7d84a2a61e1b204c0e30b1b4
Michael Heinrich <michael.heinrich@outlook.com>
2022-12-27 20:16:05 -0800

    Final version

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ fn main() {
         println!("Guess the number: ");
         let mut read_guess = String::new();
 
-        io::stdin().read_line(&mut read_guess).expect("Error reading from the command line.");
+        io::stdin()
+            .read_line(&mut read_guess)
+            .expect("Error reading from the command line.");
 
         let guess = match read_guess.trim().parse::<u32>() {
 
